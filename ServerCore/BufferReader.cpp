352840564aac5e40ff06fe3395c63fc9ef7e8c5d
SeungmoHan <gkstmdah97@naver.com>
2023-03-04 18:24:12 +0900

    Buffer Reader Buffer Writer

---
+++ ServerCore/BufferReader.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "BufferReader.h"
+
+
+/*---------------
+   BufferReader
+ --------------*/
+BufferReader::BufferReader()
+{
+}
+
+BufferReader::BufferReader(BYTE* buffer, uint32 size, uint32 pos)
+{
+}
+
+BufferReader::~BufferReader()
+{
+}
+
+BYTE* BufferReader::Buffer() const { return m_Buffer; }
+
+uint32 BufferReader::Size() const { return m_Size; }
+
+uint32 BufferReader::ReadSize() const { return m_Pos; }
+
+uint32 BufferReader::FreeSize() const { return m_Size - m_Pos; }
